d8679f7f81a680a23b3b36f714b687986f29d14e
martiponce <marponce165@gmail.com>
2025-10-22 17:23:57 -0300

    ADD: Ejercicio 7.1: Conversor de temperatura

---
+++ src/Conversor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author marpo
+ */
+public class Conversor {
+    public double celsiusAFahrenheit(double celsius) {
+        // Fórmula: (C * 9/5) + 32
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    public double fahrenheitACelsius(double fahrenheit) {
+        // Fórmula: (F - 32) * 5/9
+        return (fahrenheit - 32) * 5.0 / 9.0;
+    }
+}
